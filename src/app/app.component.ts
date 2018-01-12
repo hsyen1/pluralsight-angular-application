30f39cfda0cc4e402d2c154302f5926285502cb2
hsyen1 <hsyen1@student.monash.edu>
2018-01-12 15:37:57 +0800

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+/*
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular: Getting Started';
+}
+*/
+import { Component } from '@angular/core';
+
+@Component({
+  // metadata that defines the component
+  selector: 'pm-root',
+  template: `
+    <div><h1>{{pageTitle}}</h1>
+      <div>My First Component</div>
+    </div>
+  `
+})
+export class AppComponent {
+  pageTitle: string = 'Acme Product Management';
+}
