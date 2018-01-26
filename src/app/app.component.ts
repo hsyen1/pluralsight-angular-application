633ac0bb7051b3bd2b901c50b811f845bd268d39
hsyen1 <hsyen1@student.monash.edu>
2018-01-26 16:39:11 +0800

    - added providers and comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,28 @@
 /*
-import { Component } from '@angular/core';
+Component = Template + Class + Metadata
 
-@Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'Angular: Getting Started';
-}
-*/
+Template: View layout, HTML, binding and directives
+Class; Code supporting the view, typescript, properties and methods (for an e.g, AppComponent)
+Metadata: Extra data for Angular, defined with decorator (a function that adds metadata to a class, its members or its method arguments)
+* */
 import { Component } from '@angular/core';
+import {ProductService} from './products/product.service';
 
+//  @Component decorator that defines the metadata
+// a component must always have a template
+// double curly braces indicates curly braces
 @Component({
-  // metadata that defines the component
+  // metadata includes template
   selector: 'pm-root',
+  // {{pageTitle}} is an example of interpolation
   template: `
     <div><h1>{{pageTitle}}</h1>
-      <div>My First Component</div>
+      <pm-products></pm-products>
     </div>
-  `
+  `,
+  providers: [ProductService]
 })
+// Class
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';
 }
