31e53605da673338352b2e8ad9852a618357b6bf
hsyen1 <hsyen1@student.monash.edu>
2018-01-27 17:39:24 +0800

    - imported WelcomeComponent and ProductDetailComponent
    - configured the routes for each component (Product List, Product and Home)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ProductListComponent } from './products/product-list.component';
 import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
-import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
-import {StarComponent} from './shared/star.component';
-import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import {RouterModule} from '@angular/router';
 
 //  class that defines this module
 @NgModule({
@@ -13,12 +16,22 @@ import {HttpClientModule} from '@angular/common/http';
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    // route configuration
+    // order matters
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
